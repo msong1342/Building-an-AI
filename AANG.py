481cc03ee88c71260bd56d80dbdce37fe4d4c3f8
msong1342 <153876463+msong1342@users.noreply.github.com>
2024-12-06 19:22:47 -0700

    Delete AANG.py

--- AANG.py
+++
@@ -1,9 +0,0 @@
-# @title Setup
-from google.cloud import bigquery
-from google.colab import data_table
-import bigframes.pandas as bpd
-
-project = 'master-sector-441702-r9' # Project ID inserted based on the query results selected to explore
-location = 'US' # Location inserted based on the query results selected to explore
-client = bigquery.Client(project=project, location=location)
-data_table.enable_dataframe_formatter()
